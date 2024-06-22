ab3b08611d24eaf4ec538caa99bc50a59ce806ad
Abdurakhman <abdurahmansamatovic@gmail.com>
2024-06-22 14:37:40 +0600

    first commit

---
+++ src/app/api.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export const apiRoot = axios.create({
+    baseURL: 'as'
+})
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './app/style/index.css'
+import { Provider } from 'react-redux'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app/router/router.jsx'
+import { store } from './app/store.js'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
+  </React.StrictMode>,
+)
