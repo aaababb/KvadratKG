8a1c5e36e977e2c6297be02f4f6993157947e471
Begimai <bahadurybabegimai@gmail.com>
2024-06-24 22:28:15 +0600

    accordion

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Products from '../../features/Products/ui/Products'
+import Accordion from '../../features/Accordion/ui/Accordion'
 
 const HomePage = () => {
   return (
     <div>
       <Products/>
+      <Accordion/>
     </div>
   )
 }
