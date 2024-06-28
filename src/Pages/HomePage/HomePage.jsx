85222a455ab63dccd49aa1638b00c6066e2332ec
TM <temenovbekzan@gmail.com>
2024-06-28 10:28:30 +0600

    uslugi

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Products from '../../features/Products/ui/Products'
 import Carousel from '../../features/Banner.jsx/ui/Carousel/Carousel'
+import Uslugi from '../../features/Uslugi/Uslugi'
 
 const HomePage = () => {
   return (
     <div>
       <Carousel/>
+      <Uslugi/>
       <Products/>
     </div>
   )
