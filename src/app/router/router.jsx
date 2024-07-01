cb956b4dbbb279c80ea9787bd3954171fe26bebe
TM <temenovbekzan@gmail.com>
2024-07-01 12:30:50 +0600

    info

--- src/app/router/router.jsx
+++ src/app/router/router.jsx
@@ -3,6 +3,7 @@ import Layout from "../../widgets/layout/Layout";
 import NotFound from "../../widgets/NotFound/NotFound";
 import HomePage from "../../Pages/HomePage/HomePage";
 import CompanyPage from "../../Pages/AboutCompany/CompanyPage";
+import InfoPage from "../../Pages/InfoPage/InfoPage";
 
 export const router = createBrowserRouter([
     {
@@ -18,6 +19,10 @@ export const router = createBrowserRouter([
                 path: '/AboutCompany',
                 element: <CompanyPage/>
             },
+            {
+                path:"/info",
+                element:<InfoPage/>
+            }
         ]
     }
 ])
