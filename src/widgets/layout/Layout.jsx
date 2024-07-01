e92dce115722957e18ca622a3961197b250c75c4
Abdurakhman <abdurahmansamatovic@gmail.com>
2024-07-01 21:25:23 +0600

    create scroll

--- src/widgets/layout/Layout.jsx
+++ src/widgets/layout/Layout.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
+import React, { createContext, useContext } from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import { scroller } from 'react-scroll';
+
+export const ScrollContext = createContext();
 
 const Layout = () => {
-  return (
-    <div>
-        <Header/>
-        <Outlet/>
-        <Footer/>
-    </div>
-  )
-}
+  const scrollToSection = (section) => {
+    scroller.scrollTo(section, {
+      duration: 800,
+      delay: 0,
+      smooth: 'easeInOutQuart'
+    });
+  };
 
-export default Layout
+  return (
+    <ScrollContext.Provider value={scrollToSection}>
+      <Header />
+      <Outlet />
+      <Footer />
+    </ScrollContext.Provider>
+  );
+};
+
+export default Layout;
