d424c5db1cb8d589b289963b84ae48f477b6f73f
YrYs <yryskeldi445@gmail.com>
2024-07-18 17:22:22 +0600

    post request heading

--- src/app/api.js
+++ src/app/api.js
@@ -1,5 +1,16 @@
 import axios from "axios";
 
+const BASE_URL = "http://192.168.68.133:8080";
+
 export const apiRoot = axios.create({
-    baseURL: 'http://192.168.68.126:8080'
-})
+  baseURL: BASE_URL,
+});
+
+const formDataHeaders = {
+  "Content-Type": "multipart/form-data",
+};
+
+export const axiosWithFile = axios.create({
+  baseURL: BASE_URL,
+  headers: formDataHeaders,
+});
--- src/widgets/layout/AdminLayout.jsx
+++ src/widgets/layout/AdminLayout.jsx
@@ -1,50 +1,50 @@
-import React, { useState } from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from '../Header/Header';
-import Box from '@mui/material/Box';
-import AdminNavbar from '../AdminNavBar/AdminNavbar';
-import AdminSidebar from '../AdminSideBar/AdminSideBar';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useTheme } from '@mui/material/styles';
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
+import Box from "@mui/material/Box";
+import AdminNavbar from "../AdminNavBar/AdminNavbar";
+import AdminSidebar from "../AdminSideBar/AdminSideBar";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
 
 const drawerWidth = 240;
 
 const AdminLayout = () => {
   const [mobileOpen, setMobileOpen] = useState(false);
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-      <div className='w-full fixed top-0 z-50'>
-        <Header/>
+    <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      <div className="w-full fixed top-0 z-50">
+        <Header />
       </div>
-      <AdminNavbar handleDrawerToggle={handleDrawerToggle} isOpen={mobileOpen} />
-      <Box sx={{ display: 'flex', flexGrow: 1 }}>
+      <AdminNavbar
+        handleDrawerToggle={handleDrawerToggle}
+        isOpen={mobileOpen}
+      />
+      <Box sx={{ display: "flex", flexGrow: 1 }}>
         <AdminSidebar
           open={mobileOpen}
           handleDrawerToggle={handleDrawerToggle}
-          sx={{ /* Добавьте стили по необходимости */ }}
         />
         <Box
           component="main"
           sx={{
             flexGrow: 1,
-            paddingTop: '46px',
-            paddingLeft: '55px',
-            marginLeft: isMobile ? '0' : (mobileOpen ? `${drawerWidth}px` : '0'),
-            transition: 'margin 0.3s',
-            overflowY: 'auto',
-            // border: '2px solid blue',
-            marginTop: '120px',
-            height: '800px'
+            paddingTop: "20px",
+            paddingLeft: "55px",
+            marginLeft: isMobile ? "0" : mobileOpen ? `${drawerWidth}px` : "0",
+            transition: "margin 0.3s",
+            overflowY: "auto",
+            marginTop: "120px",
           }}
         >
-          <Outlet context={{ mobileOpen }}/>
+          <Outlet context={{ mobileOpen }} />
         </Box>
       </Box>
     </Box>
