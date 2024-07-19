9d2e910c54799043b4e1b6ffd8b412d37487bea3
mahabat-17 <amanturovamahabat265@gmail.com>
2024-07-19 14:19:29 +0600

    updateAdative

---
+++ src/features/Uslugi/data.js
@@ -0,0 +1,33 @@
+ import hi from '../../shared/assets/svg/hi.svg'
+ import chekcbox from '../../shared/assets/svg/chekcbox.svg'
+ import monay from '../../shared/assets/svg/monay.svg'
+ import ava from '../../shared/assets/svg/ava.svg'
+export const services = [
+    {
+        id: 1,
+        image:  chekcbox,
+        description: 'Продать недвижимость'
+    },
+    {
+        id: 2,
+        image: hi,
+        description: 'Срочный выкуп недвижимости'
+    },
+
+    
+    {
+        id: 3,
+        image:  monay,
+        description: 'Инвестиции в недвижимость'
+    },
+    {
+        id: 3,
+        image:  monay,
+        description: 'Подобрать недвижимость'
+    },
+    {
+        id: 4,
+        image:  ava,
+        description: 'Юридическое сопровождение'
+    },
+]
